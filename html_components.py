03d8e4287df398829182b70a4858de9ebaf68f1d
abhilashpanda04 <abhilashk.isme1517@gmail.com>
2023-06-26 20:27:21 +0530

    html templates added for chat

---
+++ html_components.py
@@ -0,0 +1,44 @@
+css= '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://avatars.githubusercontent.com/u/57190912?s=400&u=72f233728b738113e08cd73d8a92cbeca41c39ca&v=4">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
