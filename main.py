5603941d1966d2e5815a011919bffde273fb7fe0
abhilashpanda04 <abhilashk.isme1517@gmail.com>
2023-06-26 20:27:55 +0530

    retriver changed to retriever as a fix

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain import FAISS
+from langchain.embeddings import OpenAIEmbeddings,HuggingFaceInstructEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from html_components import css,bot_template,user_template
+from langchain.chat_models import ChatOpenAI
 
-def get_vector_store(text):
-    pass
+def get_conversastion_chain(vectorstore):
+    llm=ChatOpenAI()
+    memory=ConversationBufferMemory(memory_key="chat_history",return_messages=True)
+    conversastion_chain=ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectorstore.as_retriever(),memory=memory)
+    return conversastion_chain
+def get_vector_store(text_chunks):
+    # embeddings=HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    embeddings=OpenAIEmbeddings(model= "text-embedding-ada-002")
+    vector_store=FAISS.from_texts(texts=text_chunks,embedding=embeddings)
+    return vector_store
 def get_text_chunks(text):
     """
     Splits a given string into chunks of 1000 characters, with a 200 character overlap
@@ -40,12 +54,32 @@ def get_pdf_text(pdf_docs):
             text+=page.extract_text()
     return text
 
+
+def handle_user_question(user_question):
+    response=st.session_state.conversastion({'question':user_question})
+    st.session_state.chat_history=response['chat_history']
+    for i , message in enumerate(st.session_state.chat_history):
+        if i%2==0:
+            st.write(user_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
 def main():
+
     load_dotenv()
     
     st.set_page_config(page_title="chat-PDF",page_icon=":books:")
-    st.header("")
-    st.text_input("Ask a question based on the PDF")
+    
+    st.write(css,unsafe_allow_html=True)
+    if "conversastion" not in st.session_state:
+        st.session_state.conversastion=None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history=None
+    
+    st.header("Chat with PDFs")
+    user_question=st.text_input("Ask a question based on the PDF")
+    if user_question:
+        handle_user_question(user_question)
+
     with st.sidebar:
         st.subheader("Your documents")
         pdf_files=st.file_uploader("Upload your files and click submit",accept_multiple_files=True)
@@ -61,7 +95,10 @@ def main():
                 st.write(text_chunks)
 
                 #create a vector store
-                vectors=get_vector_store(text_chunks)
+                vectorstore=get_vector_store(text_chunks)
+
+                st.session_state.conversastion=get_conversastion_chain(vectorstore)
+
 
 
 
